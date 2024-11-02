6a1424858bbd8381b9b08ea894b29801216c588a
Asim Osman <asim.awad98@gmail.com>
2024-11-02 23:31:35 +0200

    updated README

--- 2D_Embedding_Plot.py
+++ 2D_Embedding_Plot.py
@@ -4,16 +4,16 @@ from sklearn.decomposition import PCA
 import pickle
 import polars as pl
 import os
-# The data Structure + Helper Fucntions
+# The data Structure + Helper_Functions 
 import Helper_Functions
 # experiment_folder = "Experiments/32_million_trials/K_30_n_Epochs_40_lmbd_1_gamma_0.1_taw_10/"
-experiment_folder = "Experiments/32_million_trials/a/32_million_trial1/"
+experiment_folder = "Experiments/32_million_trials/"
 # moviess = pl.read_csv(f'ml-32m/movies.csv')
 # ratings = pl.read_csv('ml-32m/ratings.csv')
 K_factors = 30; lambda_reg = 1 ; gamma = 1 ; taw =  10 
 # # K_factors = 25 ; n_Epochs = 40 ;  lambda_reg = 20 ; gamma = 0.01 ; taw =  30 
 # ratings = pl.read_csv("ml-32m/ratings.csv")
-# train_df,test_df,user_idx_map,  movie_idx_map ,idx_to_user,idx_to_movie =  Helper.create_train_test_df(ratings)
+# train_df,test_df,user_idx_map,  movie_idx_map ,idx_to_user,idx_to_movie =  Helper_Functions.create_train_test_df(ratings)
 # print(train_df.head())
 # # Load movie titles
 # movies_df = pl.read_csv("ml-32m/movies.csv").select(["movieId", "title"])
@@ -25,11 +25,11 @@ K_factors = 30; lambda_reg = 1 ; gamma = 1 ; taw =  10
 # with open(os.path.join(experiment_folder, 'idx_to_title.pkl'), 'wb') as f:
 #     pickle.dump(idx_to_title, f)
 # # Directory where you want to save the dictionaries
-# # Helper.save_idx_maps(experiment_folder,user_idx_map,  movie_idx_map ,idx_to_user,idx_to_movie)
+# # Helper_Functions.save_idx_maps(experiment_folder,user_idx_map,  movie_idx_map ,idx_to_user,idx_to_movie)
 
 
-user_idx_map,  movie_idx_map ,idx_to_user,idx_to_movie  = Helper.Load_idx_maps(f"{experiment_folder}/model_params/")
-movies_factors,users_factors,user_bias,item_bias = Helper.load_model(experiment_folder)
+user_idx_map,  movie_idx_map ,idx_to_user,idx_to_movie  = Helper_Functions.Load_idx_maps(f"{experiment_folder}/model_params/")
+movies_factors,users_factors,user_bias,item_bias = Helper_Functions.load_model(experiment_folder)
 
 with open(os.path.join(experiment_folder, 'idx_to_title.pkl'), 'rb') as f:
     idx_to_title = pickle.load(f)
